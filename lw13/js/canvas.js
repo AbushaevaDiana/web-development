7553058e5466508afd94e4fa871ad7bb96cdb466
AbushaevaDian <abu13.di@gmail.com>
2021-06-04 03:22:46 +0300

    add second character

--- lw13/js/canvas.js
+++ lw13/js/canvas.js
@@ -81,6 +81,7 @@ function draw(){
     context.fillStyle = '#FF4500';
     context.fillRect(80, 240, 140, 55);
 
+//Обувь
     context.beginPath();
     context.moveTo(85, 315);
     context.lineTo(215, 315);
@@ -179,6 +180,272 @@ function draw(){
     context.strokeStyle = '#8B4513';
     context.stroke();
 
+
+
+
+
+    //Второй
+    //
+    //
+
+//Куртка
+    context.fillStyle = '#4169E1';
+    context.fillRect(390, 230, 120, 85);
+
+    context.fillStyle = '#CD5C5C';
+    context.fillRect(380, 240, 140, 60);
+
+//Обувь
+
+    context.beginPath();
+    context.moveTo(380, 315);
+    context.lineWidth = 1;
+    context.quadraticCurveTo(415, 305, 450, 315);
+    context.fillStyle = '#000000';
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(450, 315);
+    context.lineWidth = 1;
+    context.quadraticCurveTo(475, 305, 520, 315);
+    context.fillStyle = '#000000';
+    context.fill();
+
+//Рукава
+    context.beginPath();
+    context.ellipse(385, 255, 10, 35, Math.PI/10 , Math.PI*2, 0);
+    context.fillStyle = '#CD5C5C';
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(515, 255, 10, 35, -Math.PI/10 , Math.PI*2, 0);
+    context.fillStyle = '#CD5C5C';
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(395, 253);
+    context.lineWidth = 1;
+    context.quadraticCurveTo(389, 265, 389, 275);
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(505, 253);
+    context.lineWidth = 1;
+    context.quadraticCurveTo(511, 265, 510, 275);
+    context.strokeStyle = '#000000';
+    context.stroke();
+
+//Застежка
+    context.beginPath();
+    context.moveTo(450, 250);
+    context.lineTo(445, 300);
+    context.lineCap='butt';
+    context.lineWidth = 3;
+    context.strokeStyle = '#2F4F4F';
+    context.stroke();
+
+    context.beginPath();
+    context.arc(440, 265, 2, 0, Math.PI * 2);
+    context.fillStyle = '#2F4F4F';
+    context.fill();
+
+    context.beginPath();
+    context.arc(438, 280, 2, 0, Math.PI * 2);
+    context.fillStyle = '#2F4F4F';
+    context.fill();
+
+    context.beginPath();
+    context.arc(436, 295, 2, 0, Math.PI * 2);
+    context.fillStyle = '#2F4F4F';
+    context.fill();
+
+//Воротник
+    context.beginPath();
+    context.ellipse(420, 242, 11, 33, -7*Math.PI/3, Math.PI*2, 0);
+    context.strokeStyle = '#B22222';
+    context.stroke();
+    context.fillStyle = '#FF0000';
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(480, 247, 11, 33, 4*Math.PI/11, Math.PI*2, 0);
+    context.strokeStyle = '#B22222';
+    context.stroke();
+    context.fillStyle = '#FF0000';
+    context.fill();
+
+//Голова
+    context.beginPath();
+    context.arc(450, 150, 97, 0, Math.PI * 2);
+    context.fillStyle = '#FFFACD';
+    context.fill();
+
+//Шапка
+    context.beginPath();
+    context.arc(450, 150, 100, -7*Math.PI/8, -Math.PI/8);
+    context.strokeStyle = '#4169E1';
+    context.fillStyle = '#4169E1';
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(350, 120);
+    context.lineCap='butt';
+    context.lineWidth = 20;
+    context.quadraticCurveTo(455, 85, 550, 120);
+    context.strokeStyle = '#FF0000';
+    context.stroke();
+
+//Помпон
+    context.lineCap = 'round';
+    context.lineWidth = 5;
+    context.strokeStyle = '#FF0000';
+
+    context.beginPath();
+    context.moveTo(430, 35);
+    context.lineTo(432, 67);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(423, 40);
+    context.lineTo(440, 67);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(417, 45);
+    context.lineTo(447, 62);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(415, 53);
+    context.lineTo(447, 52);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(417, 60);
+    context.lineTo(447, 45);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(438, 38);
+    context.lineTo(425, 67);
+    context.stroke();
+
+//Глаза
+    context.beginPath();
+    context.ellipse(475, 148, 25, 30, -Math.PI/6, Math.PI*2, 0);
+    context.fillStyle = '#FFFFFF';
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(425, 145, 25, 30, Math.PI/6 , Math.PI*2, 0);
+    context.fillStyle = '#FFFFFF';
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(425, 145, 25, 30, Math.PI/6 , -5*(Math.PI/6), -(Math.PI/4));
+    context.lineWidth = 1/2;
+    context.strokeStyle = '#DEB887';
+    context.stroke();
+
+    context.beginPath();
+    context.ellipse(475, 148, 25, 30, -Math.PI/6 , -5*(Math.PI/6), -(Math.PI/5));
+    context.strokeStyle = '#DEB887';
+    context.stroke();
+
+    context.beginPath();
+    context.arc(430, 145, 4, 0, Math.PI * 2);
+    context.fillStyle = '#000000';
+    context.fill();
+
+    context.beginPath();
+    context.arc(468, 148, 4, 0, Math.PI * 2);
+    context.fillStyle = '#000000';
+    context.fill();
+
+//Брови
+    context.lineCap = 'butt';
+    context.lineWidth = 3;
+    context.strokeStyle = '#000000';
+
+    context.beginPath();
+    context.moveTo(380, 125);
+    context.lineTo(400, 107);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(510, 125);
+    context.lineTo(490, 107);
+    context.stroke();
+
+//Рот
+    context.beginPath();
+    context.moveTo(450, 235);
+    context.lineTo(425, 215);
+    context.lineTo(475, 215);
+    context.lineTo(450, 235);
+    context.lineCap='round';
+    context.lineWidth = 3;
+    context.fillStyle = '#FFFFFF';
+    context.strokeStyle = '#000000';
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.moveTo(450, 235);
+    context.lineTo(433, 222);
+    context.lineTo(467, 222);
+    context.lineCap='round';
+    context.lineWidth = 3;
+    context.fillStyle = '#000000';
+    context.fill();
+
+    context.lineCap='round';
+    context.lineWidth = 2;
+    context.strokeStyle = '#000000';
+
+    context.beginPath();
+    context.moveTo(450, 215);
+    context.lineTo(451, 225);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(441, 215);
+    context.lineTo(439, 225);
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(463, 215);
+    context.lineTo(460, 225);
+    context.stroke();
+
+//Перчатки
+    context.beginPath();
+    context.ellipse(525, 275, 15, 17, -Math.PI/6, Math.PI*2, 0);
+    context.fillStyle = '#FF0000';
+    context.fill();
+
+    context.beginPath();
+    context.ellipse(375, 275, 15, 17, Math.PI/6, Math.PI*2, 0);
+    context.fillStyle = '#FF0000';
+    context.fill();
+
+    context.beginPath();
+    context.arc(510, 270, 6, 0, Math.PI * 2);
+    context.lineWidth = 1;
+    context.strokeStyle = '#B22222';
+    context.fillStyle = '#FF0000';
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(390, 270, 6, 0, Math.PI * 2);
+    context.lineWidth = 1;
+    context.strokeStyle = '#B22222';
+    context.fillStyle = '#FF0000';
+    context.fill();
+    context.stroke();
 }
 
 window.onload = () => draw();
